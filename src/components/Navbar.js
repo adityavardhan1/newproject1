e22a39e06f9df26ce29a1b61118e037889c92508
Yuvraj Vardhan <vardhan.yuvraj.singh@gmail.com>
2025-03-30 01:00:48 +0530

    added dropdown feature in calculator

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,30 @@
 import "./Navbar.css";
-import{ Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 export default function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const calculators = [
+    { name: 'SIP Growth Calculator', path: './Calculator/Sip' },
+    { name: 'Lumpsum Calculator', path: '/./Calculator/lumpsum' }
+  ];
+
+  const handleCalculatorClick = () => {
+    setShowDropdown(false);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
       <div className="navbar-left">
         <h1 className="Logo">NitiDhan</h1>
       </div>
@@ -10,7 +32,26 @@ export default function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/ContactUs">Support Us</Link>
         <Link to="/AskUs">Ask Us</Link>
-        <Link to="/Calculator">Calculator</Link>
+        <div 
+          className="calculator-dropdown"
+          onMouseEnter={() => setShowDropdown(true)}
+          onMouseLeave={() => setShowDropdown(false)}
+        >
+          <Link to="/Calculator">Calculator</Link>
+          {showDropdown && (
+            <div className="dropdown-content">
+              {calculators.map(calc => (
+                <Link 
+                  key={calc.path} 
+                  to={calc.path}
+                  onClick={handleCalculatorClick}
+                >
+                  {calc.name}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
         <Link to="/FIRE">F.I.R.E</Link>
         <Link to="/Market">Market</Link>
         <Link to="/Login">Login</Link>
